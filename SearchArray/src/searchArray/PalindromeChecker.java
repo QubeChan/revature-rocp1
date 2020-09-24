672ade74b1026278415d1f134611f182cd77edcf
QubeChan <jchan122490@gmail.com>
2020-09-23 22:48:03 -0400

    fixed up palindrome checker it now works

--- SearchArray/src/searchArray/PalindromeChecker.java
+++ SearchArray/src/searchArray/PalindromeChecker.java
@@ -4,11 +4,23 @@ public class PalindromeChecker {
 
     public static void main(String[] args) {
         PalindromeChecker pc = new PalindromeChecker();
-        pc.isPalindrome("Racear");
+        pc.isPalindrome("Racecar");
 
     }
 
     public void isPalindrome(String s) {
+        String reverseString = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reverseString = reverseString + s.charAt(i);
+        }
 
+//        System.out.println(s);
+//        System.out.println(reverseString);
+        
+        if (s.equalsIgnoreCase(reverseString)) {
+            System.out.println(s + " is a palindrome");
+        } else {
+            System.out.println(s + " is not a palindrome");
+        }
     }
 }
